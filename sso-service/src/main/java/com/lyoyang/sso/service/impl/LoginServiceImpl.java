d8e16b4a41dbdd9ef3d0b71084f90bc597896eed
yangbing <bing.yang@clinks.com.cn>
2018-05-11 17:24:32 +0800

    sso

--- sso-service/src/main/java/com/lyoyang/sso/service/impl/LoginServiceImpl.java
+++ sso-service/src/main/java/com/lyoyang/sso/service/impl/LoginServiceImpl.java
@@ -1,4 +1,40 @@
 package com.lyoyang.sso.service.impl;
 
-public class LoginServiceImpl {
+import com.lyoyang.entity.User;
+import com.lyoyang.service.LoginService;
+import com.lyoyang.sso.dao.UserMapper;
+import com.lyoyang.utils.JedisClient;
+import com.lyoyang.utils.JsonReturn;
+import com.lyoyang.utils.JsonUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.util.DigestUtils;
+
+import java.util.UUID;
+
+public class LoginServiceImpl implements LoginService {
+
+    @Autowired
+    private JedisClient jedisClient;
+
+    @Autowired
+    private UserMapper userMapper;
+
+    @Value("${SESSION_EXPIRE}")
+    private Integer SESSION_EXPIRE;
+    @Override
+    public JsonReturn toLogin(String username, String password) {
+        User user = userMapper.getByUserName(username);
+        if(user == null) {
+            return JsonReturn.errorInstance("用户名或密码错误");
+        }
+        if(!DigestUtils.md5DigestAsHex(password.getBytes()).equals(user.getPassword())) {
+            return JsonReturn.errorInstance("用户名或密码错误");
+        }
+        String token = UUID.randomUUID().toString();
+        user.setPassword(null);
+        jedisClient.set("SESSION:" + token, JsonUtils.objectToJson(user));
+        jedisClient.expire("SESSION:" + token, SESSION_EXPIRE);
+        return JsonReturn.successInstance(token);
+    }
 }
--- sso-service/src/main/java/com/lyoyang/sso/service/impl/TokenServiceImpl.java
+++ sso-service/src/main/java/com/lyoyang/sso/service/impl/TokenServiceImpl.java
@@ -1,4 +1,30 @@
 package com.lyoyang.sso.service.impl;
 
-public class TokenServiceImpl {
+import com.lyoyang.entity.User;
+import com.lyoyang.service.TokenService;
+import com.lyoyang.utils.JedisClient;
+import com.lyoyang.utils.JsonReturn;
+import com.lyoyang.utils.JsonUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.util.StringUtils;
+
+public class TokenServiceImpl implements TokenService {
+
+
+    @Autowired
+    private JedisClient jedisClient;
+
+    @Value("${SESSION_EXPIRE}")
+    private Integer SESSION_EXPIRE;
+
+    @Override
+    public JsonReturn getUserByToken(String token) {
+        String json = jedisClient.get("SESSION:" + token);
+        if(StringUtils.isEmpty(json)) {
+            return JsonReturn.createSuccessResult("201","用户登录过期，请重新登录");
+        }
+        jedisClient.expire("SESSION:" + token, SESSION_EXPIRE);
+        return JsonReturn.successInstance(JsonUtils.jsonToPojo(json, User.class));
+    }
 }
